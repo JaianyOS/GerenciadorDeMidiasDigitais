8781d3e1d46714e71beacbb2ad3b7e19ef26c461
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-05-04 13:19:08 -0300

    add metodos abstratos no audiobook

--- src/midia/models/AudiobookCapitulo.java
+++ src/midia/models/AudiobookCapitulo.java
@@ -35,4 +35,44 @@ public class AudiobookCapitulo extends ArquivoMidia {
     public void setNumeroCapitulo(int numeroCapitulo) {
         this.numeroCapitulo = numeroCapitulo;
     }
+
+    // implementacao metodos abstratos
+
+    @Override
+    public String getDetalhesExibicao() {
+        return String.format("AUDIOBOOK || 📗 Livro: %s (%s) | Cap. %d: %s | Duração: %s | Favorito: %s",
+                nomeLivro,
+                nomeAutor,
+                numeroCapitulo,
+                getTitulo(),
+                formatarDuracao(getDuracaoSegundos()),
+                isFavorito() ? "Sim" : "Não");
+    }
+
+    @Override
+    public void play() {
+        System.out.printf("▶️ Tocando Audiobook: '%s' por %s - Cap. %d: '%s' (%s)...%n",
+                nomeLivro,
+                nomeAutor,
+                numeroCapitulo,
+                getTitulo(),
+                formatarDuracao(getDuracaoSegundos())
+        );
+    }
+
+    // metodo privado p auxiliar na formatacao da dduracao de segundos
+
+    private String formatarDuracao(int totalSegundos) {
+        if (totalSegundos < 0)
+            return "00:00";
+
+        long horas = totalSegundos / 3600;
+        long minutos = (totalSegundos % 3600) / 60;
+        long segundos = totalSegundos % 60;
+
+        if (horas > 0)
+            return String.format("%02d:%02d:%02d", horas, minutos, segundos);
+        else
+            return String.format("%02d:%02d", minutos, segundos);
+    }
 }
